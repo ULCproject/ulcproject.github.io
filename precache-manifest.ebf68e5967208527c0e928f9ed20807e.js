d9aff02b7d7745e64901b2b2a9e70b46694b1c0d
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-09-10 10:28:12 -0500

    Updates

---
+++ precache-manifest.ebf68e5967208527c0e928f9ed20807e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a38ce9d29880470f27116a2e95d6bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a6fe5e5d5425ebe0ec",
+    "url": "/static/css/main.f0cae126.chunk.css"
+  },
+  {
+    "revision": "a568a8ffd8e77ffcc26a",
+    "url": "/static/js/2.87752542.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87752542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a6fe5e5d5425ebe0ec",
+    "url": "/static/js/main.76d4d41b.chunk.js"
+  },
+  {
+    "revision": "405ceb2aeadcfd52a835",
+    "url": "/static/js/runtime-main.a280ac8f.js"
+  }
+]);
